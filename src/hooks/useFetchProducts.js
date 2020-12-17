7873f2fba028dcc7b2564d0299647886c62270e7
arriaga <arriaga.luis.j@gmail.com>
2020-12-17 11:03:08 -0600

    Update useFetchProducts.js

--- src/hooks/useFetchProducts.js
+++ src/hooks/useFetchProducts.js
@@ -5,16 +5,13 @@ import {getProducts} from "../helpers/getProducts";
 export  const useFetchProducts = (product) => {
 
 
-    console.log("useFetchProducts");
-    console.log(product);
-
     const [state , setState] = useState({
         data: [],
         loading: true
     });
 
     useEffect(() => {
-            console.log("Entro");
+      
             getProducts(product, 1).then( product => {
                 setState({
                     data: product,
